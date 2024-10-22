8d49bdcc83ff62984c1a1c66cd6155e6db0776bb
Max <githubmaavel@gmail.com>
2024-10-22 19:34:33 +0600

    fix

--- src/pages/registration/registration.jsx
+++ src/pages/registration/registration.jsx
@@ -1,35 +1,37 @@
 import { useState, useEffect } from "react";
+import classes from "./registration.module.scss";
 import {
   registerUser,
   loginUser,
   logoutUser,
   loginWithGoogle,
   sendVerificationEmail,
-  auth, // Импортируйте auth
+  auth,
 } from "../../base/base";
 import { useNavigate } from "react-router-dom";
 import { Button, Form, Input } from "antd";
-import * as Yup from "yup"; // Импортируем Yup
-import { Formik } from "formik"; // Импортируем Formik
-import { onAuthStateChanged } from "firebase/auth"; // Импортируйте onAuthStateChanged
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { onAuthStateChanged } from "firebase/auth";
+import { ReactTyped } from "react-typed";
+import google from "../../assets/Google.svg";
 
 const Registration = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [error, setError] = useState("");
+  const [isAnimationActive, setIsAnimationActive] = useState(false);
   const navigate = useNavigate();
 
-  // Слушатель для проверки статуса аутентификации
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        navigate("/profile"); // Редирект на профиль после успешного входа
+        navigate("/profile");
       }
     });
 
     return () => unsubscribe();
   }, [navigate]);
 
-  // Создаем схему валидации с помощью Yup
   const validationSchema = Yup.object().shape({
     email: Yup.string()
       .email("Неверный формат email")
@@ -46,11 +48,10 @@ const Registration = () => {
     const { email, password } = values;
 
     if (isLogin) {
-      // Логика для входа
       loginUser(email, password)
         .then((user) => {
           if (user.emailVerified) {
-            navigate("/profile"); // Редирект на профиль после успешного входа
+            navigate("/profile");
           } else {
             setError("Email не подтвержден. Пожалуйста, проверьте почту.");
           }
@@ -59,13 +60,12 @@ const Registration = () => {
           setError(`Ошибка входа: ${error.message}`);
         });
     } else {
-      // Логика для регистрации
       registerUser(email, password)
         .then((user) => {
-          sendVerificationEmail(user); // Отправка письма для подтверждения
+          sendVerificationEmail(user);
           setError("Регистрация успешна! Проверьте почту для подтверждения.");
           setTimeout(() => {
-            navigate("/profile"); // Редирект на профиль после регистрации
+            navigate("/profile");
           }, 3000);
         })
         .catch((error) => {
@@ -74,76 +74,131 @@ const Registration = () => {
     }
   };
 
+  const toggleForm = () => {
+    setIsAnimationActive(true);
+    setTimeout(() => {
+      setIsLogin(!isLogin);
+      setIsAnimationActive(false);
+    }, 500);
+  };
+
   return (
-    <div>
-      <h2>{isLogin ? "Вход" : "Регистрация"}</h2>
-      <Formik
-        initialValues={{ email: "", password: "", confirmPassword: "" }}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
+    <div className={classes.formPage}>
+      <div className={classes.textMarketing}>
+        <h1>
+          <span className={classes.titleSpan}>Начнем</span> создавать&nbsp;
+          <ReactTyped
+            className={classes.ActiveText}
+            strings={[
+              "корпоративные сайты",
+              "интернет-магазины",
+              "лендинги",
+              "блоги",
+              "портфолио",
+            ]}
+            typeSpeed={50}
+            backSpeed={30}
+            loop
+          />
+        </h1>
+      </div>
+      <div
+        className={`${classes.formContainer} ${
+          isAnimationActive ? "" : classes.show
+        }`}
       >
-        {({ handleSubmit, handleChange, values, errors }) => (
-          <Form onFinish={handleSubmit} autoComplete="off">
-            <Form.Item
-              label="Email"
-              name="email"
-              validateStatus={errors.email ? "error" : ""}
-              help={errors.email}
-            >
-              <Input
-                name="email"
-                onChange={handleChange}
-                value={values.email}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Пароль"
-              name="password"
-              validateStatus={errors.password ? "error" : ""}
-              help={errors.password}
-            >
-              <Input.Password
-                name="password"
-                onChange={handleChange}
-                value={values.password}
-              />
-            </Form.Item>
-
-            {!isLogin && (
+        <Formik
+          initialValues={{ email: "", password: "", confirmPassword: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ handleSubmit, handleChange, values, errors }) => (
+            <Form onFinish={handleSubmit} autoComplete="off">
+              <h2 className={classes.titleForm}>
+                {isLogin ? "Вход" : "Регистрация"}
+              </h2>
               <Form.Item
-                label="Подтвердите пароль"
-                name="confirmPassword"
-                validateStatus={errors.confirmPassword ? "error" : ""}
-                help={errors.confirmPassword}
+                label="Email"
+                name="email"
+                validateStatus={errors.email ? "error" : ""}
+                help={errors.email}
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+                className={classes.customLabel}
               >
-                <Input.Password
-                  name="confirmPassword"
+                <Input
+                  name="email"
+                  className={classes.customInput}
                   onChange={handleChange}
-                  value={values.confirmPassword}
+                  value={values.email}
                 />
               </Form.Item>
-            )}
 
-            {error && <p style={{ color: "red" }}>{error}</p>}
+              <Form.Item
+                label="Пароль"
+                name="password"
+                validateStatus={errors.password ? "error" : ""}
+                help={errors.password}
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+                className={classes.customLabel}
+              >
+                <Input.Password
+                  name="password"
+                  className={classes.customInput}
+                  onChange={handleChange}
+                  value={values.password}
+                />
+              </Form.Item>
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                {isLogin ? "Войти" : "Зарегистрироваться"}
-              </Button>
-            </Form.Item>
-          </Form>
-        )}
-      </Formik>
+              <div className="login">
+                {!isLogin && (
+                  <Form.Item
+                    className={classes.customLabel}
+                    label="Подтвердите пароль"
+                    name="confirmPassword"
+                    validateStatus={errors.confirmPassword ? "error" : ""}
+                    help={errors.confirmPassword}
+                    labelCol={{ span: 24 }}
+                    wrapperCol={{ span: 24 }}
+                  >
+                    <Input.Password
+                      className={classes.customInput}
+                      name="confirmPassword"
+                      onChange={handleChange}
+                      value={values.confirmPassword}
+                    />
+                  </Form.Item>
+                )}
+              </div>
 
-      <p>
-        {isLogin ? "У вас еще нет аккаунта? " : "У вас уже есть аккаунт? "}
-        <Button type="link" onClick={() => setIsLogin(!isLogin)}>
-          {isLogin ? "Зарегистрироваться" : "Войти"}
-        </Button>
-      </p>
+              {error && <p style={{ color: "red" }}>{error}</p>}
 
-      <Button onClick={loginWithGoogle}>Войти через Google</Button>
+              <div className={classes.btnSubmitForm}>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">
+                    {isLogin ? "Войти" : "Зарегистрироваться"}
+                  </Button>
+                </Form.Item>
+              </div>
+              <div className={classes.formLinks}>
+                <p>
+                  {isLogin
+                    ? "У вас еще нет аккаунта? "
+                    : "У вас уже есть аккаунт? "}
+                  <a type="link" onClick={toggleForm}>
+                    {isLogin ? "Зарегистрироваться" : "Войти"}
+                  </a>
+                </p>
+                <span className={classes.or}>ИЛИ</span>
+                <Button onClick={loginWithGoogle}>
+                  <img src={google} alt="google" /> Войти через Google
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
