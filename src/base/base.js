22c04e5a8e9757c832513fd83c8d58212b9b1154
Max <githubmaavel@gmail.com>
2024-10-22 19:53:18 +0600

    fff

--- src/base/base.js
+++ src/base/base.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import {
   getAuth,
   GoogleAuthProvider,
-  signInWithRedirect,
+  signInWithPopup,
   getRedirectResult,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
