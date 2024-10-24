db257aa6bbfb1945385f1a55ba8d8db34f08693d
Max <githubmaavel@gmail.com>
2024-10-25 01:03:08 +0600

    fix auth

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Loader from "../../UI/Loader/loader";
 
 const PrivateRoute = ({ element }) => {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const [isAuthenticated, setIsAuthenticated] = React.useState(null); // Установим начальное значение null
+  const [loading, setLoading] = React.useState(true); // Добавим состояние загрузки
 
   React.useEffect(() => {
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setIsAuthenticated(!!user);
+      setLoading(false); // Установим загрузку в false после проверки
     });
 
     return () => unsubscribe();
   }, []);
 
+  // Если состояние загрузки еще не установлено, показываем индикатор загрузки
+  if (loading) {
+    return <Loader />; // Вы можете заменить это на свой индикатор загрузки
+  }
+
+  // Перенаправляем в зависимости от состояния аутентификации
   return isAuthenticated ? element : <Navigate to="/authorization" />;
 };
 
